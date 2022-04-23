0cafa705b03b1c7fc169767c1576fbbbb58738d0
maxcore25 <mkorobov2002@mail.ru>
2022-04-23 15:31:43 +0300

    Add Product Endpoint Added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import { useGetGoodsQuery } from './redux';
 
 function App() {
   const [count, setCount] = useState('');
-  const { data = [], isLoading } = useGetGoodsQuery();
+  const { data = [], isLoading } = useGetGoodsQuery(count);
 
   if (isLoading) return <h1>Loading...</h1>;
 
   return (
     <div className='App'>
       <header className='App-header'>
+        <div>
+          <select value={count} onChange={e => setCount(e.target.value)}>
+            <option value='' key='all'>
+              all
+            </option>
+            <option value='1' key='1'>
+              1
+            </option>
+            <option value='2' key='2'>
+              2
+            </option>
+            <option value='3' key='3'>
+              3
+            </option>
+          </select>
+        </div>
         <ul>
           {data.map(item => (
             <li key={item.id}>{item.name}</li>
--- src/redux/goodsApi.js
+++ src/redux/goodsApi.js
@@ -7,7 +7,14 @@ export const goodsApi = createApi({
     getGoods: build.query({
       query: (limit = '') => `goods?${limit && `_limit=${limit}`}`,
     }),
+    addProduct: build.mutation({
+      query: body => ({
+        url: 'goods',
+        method: 'POST',
+        body,
+      }),
+    }),
   }),
 });
 
-export const { useGetGoodsQuery } = goodsApi;
+export const { useGetGoodsQuery, useAddProductMutation } = goodsApi;
