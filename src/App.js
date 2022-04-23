ee474b19e9505dcbd11a165dce9c9c46afa2f4e8
maxcore25 <mkorobov2002@mail.ru>
2022-04-23 16:01:49 +0300

    Delete Endpoint Tested

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { useState } from 'react';
-import { useAddProductMutation, useGetGoodsQuery } from './redux';
+import {
+  useAddProductMutation,
+  useDeleteProductMutation,
+  useGetGoodsQuery,
+} from './redux';
 
 function App() {
   const [count, setCount] = useState('');
   const [newProduct, setNewProduct] = useState('');
   const { data = [], isLoading } = useGetGoodsQuery(count);
   const [addProduct, { isError }] = useAddProductMutation();
+  const [deleteProduct] = useDeleteProductMutation();
 
   const handleAddProduct = async () => {
     if (newProduct) {
       await addProduct({ name: newProduct }).unwrap();
       setNewProduct('');
     }
+  };
+
+  const handleDeleteProduct = async id => {
+    await deleteProduct(id).unwrap();
   };
 
   if (isLoading) return <h1>Loading...</h1>;
@@ -45,7 +54,9 @@ function App() {
         </div>
         <ul>
           {data.map(item => (
-            <li key={item.id}>{item.name}</li>
+            <li key={item.id} onClick={() => handleDeleteProduct(item.id)}>
+              {item.name}
+            </li>
           ))}
         </ul>
       </header>
