bdf07c028bdb5f962a009c41cdecef2b5880847f
maxcore25 <mkorobov2002@mail.ru>
2022-04-23 14:53:24 +0300

    Goods API Started

---
+++ src/redux/goodsApi.js
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const goodsApi = createApi({
+  reducerPath: 'goodsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
+  endpoints: build => ({
+    getGoods: build.query({
+      query: () => `goods`,
+    }),
+  }),
+});
