9eac0aaf92811196ba00c536a61490f730134f2d
maxcore25 <mkorobov2002@mail.ru>
2022-04-23 15:44:13 +0300

    POST Query Added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import { useState } from 'react';
-import { useGetGoodsQuery } from './redux';
+import { useAddProductMutation, useGetGoodsQuery } from './redux';
 
 function App() {
   const [count, setCount] = useState('');
+  const [newProduct, setNewProduct] = useState('');
   const { data = [], isLoading } = useGetGoodsQuery(count);
+  const [addProduct, { isError }] = useAddProductMutation();
+
+  const handleAddProduct = async () => {
+    if (newProduct) {
+      await addProduct({ name: newProduct }).unwrap();
+      setNewProduct('');
+    }
+  };
 
   if (isLoading) return <h1>Loading...</h1>;
 
   return (
     <div className='App'>
       <header className='App-header'>
+        <div>
+          <input
+            type='text'
+            value={newProduct}
+            onChange={e => setNewProduct(e.target.value)}
+          />
+          <button onClick={handleAddProduct}>Add Product</button>
+        </div>
         <div>
           <select value={count} onChange={e => setCount(e.target.value)}>
             <option value='' key='all'>
