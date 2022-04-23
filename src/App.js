5aaf891936afe73d5930522bec9b40291746b470
maxcore25 <mkorobov2002@mail.ru>
2022-04-23 15:16:15 +0300

    Get Goods Endpoint Works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useGetGoodsQuery } from './redux';
+
 function App() {
+  const { data = [], isLoading } = useGetGoodsQuery();
+
+  if (isLoading) return <h1>Loading...</h1>;
+
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header className='App-header'>
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </header>
     </div>
   );
 }
