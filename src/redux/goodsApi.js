552a2730aa74b59a109dcfbdb9c429c9f1d54e2b
maxcore25 <mkorobov2002@mail.ru>
2022-04-23 15:52:24 +0300

    Auto Fetch Refresh Added

--- src/redux/goodsApi.js
+++ src/redux/goodsApi.js
@@ -2,10 +2,18 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const goodsApi = createApi({
   reducerPath: 'goodsApi',
+  tagTypes: ['Products'],
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
   endpoints: build => ({
     getGoods: build.query({
       query: (limit = '') => `goods?${limit && `_limit=${limit}`}`,
+      providesTags: result =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: 'Products', id })),
+              { type: 'Products', id: 'LIST' },
+            ]
+          : [{ type: 'Products', id: 'LIST' }],
     }),
     addProduct: build.mutation({
       query: body => ({
@@ -13,6 +21,7 @@ export const goodsApi = createApi({
         method: 'POST',
         body,
       }),
+      invalidatesTags: [{ type: 'Products', id: 'LIST' }],
     }),
   }),
 });
